a70893b91a631ab7626093ad24c5729bf5f938b1
unknown <thisiskakonray@gmail.com>
2024-02-17 21:52:05 +0600

    course admission page responsive

--- resources/views/users/course_admission.blade.php
+++ resources/views/users/course_admission.blade.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="card p-5">
+                    <div class="card p-lg-5 p-3">
                         <div class="text-center pb-5">
                             <h6 class="pb-2 heading2"> Fill the form to participate any course</h6>
                         </div>
@@ -84,7 +84,7 @@
                     <div id="map-container-google-2">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29424.01342927033!2d89.5338316883197!3d22.802401689040643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff8ff9603b90bf%3A0xbd8858d11188c418!2zTmlyYWxhIE1vcmUsIOCmluCngeCmsuCmqOCmvg!5e0!3m2!1sbn!2sbd!4v1704436117921!5m2!1sbn!2sbd"
-                             height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                             height="450" width="100%" style="border:0;" allowfullscreen="" loading="lazy"
                             referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
 
